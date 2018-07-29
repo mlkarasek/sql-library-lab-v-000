30ddc87fa752ee4ca3e005dfb4040e137bc16a74
Monica  Karasek <mlkarasek@gmail.com>
2018-07-29 22:41:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species FROM characters
+  "SELECT species COUNT(*) FROM characters
   GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1";
 end
 
