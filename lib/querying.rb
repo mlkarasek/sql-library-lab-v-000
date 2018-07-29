39ba2f7d8ac7996ecb309f7e34d37666063bb7b9
Monica  Karasek <mlkarasek@gmail.com>
2018-07-29 22:55:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,8 +23,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title FROM series
   JOIN characters ON characters.series_id = series.id
-  WHERE characters.species = human
-  ORDER BY characters.species DESC LIMIT 1"
+  WHERE characters.species = 'human'
+  ORDER BY characters.species DESC LIMIT 1";
 end
 
 def select_character_names_and_number_of_books_they_are_in
