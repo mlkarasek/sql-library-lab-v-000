5c7418e4e96c043cc4baec121b0509c9ae628614
Monica  Karasek <mlkarasek@gmail.com>
2018-07-29 22:51:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,13 +16,14 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors
+  JOIN series ON series.author_id  = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id";
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT"
 end
