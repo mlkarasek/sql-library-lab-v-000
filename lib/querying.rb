9ac36ebdcabc501cf6935d026ba6b989f88d960f
Monica  Karasek <mlkarasek@gmail.com>
2018-07-29 22:53:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT species FROM characters 
+  "SELECT species FROM characters
   WHERE characters.species = human
   ORDER BY characters.species DESC LIMIT 1"
 end
