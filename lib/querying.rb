8d1bd3b4a3356cc40ffc45d2094895be89517a9f
Monica  Karasek <mlkarasek@gmail.com>
2018-07-29 22:58:07 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,5 +28,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT"
+  "SELECT character.name COUNT(character_books.character_id) FROM characters
+  JOIN character_books ON character.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_books.character_id)";
 end
